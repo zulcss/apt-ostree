03f0c8267f8f6c47ba0b55b3582282e3df28dffd
Charles Short <charles.short@windriver.com>
2023-09-12 20:32:11 -0400

    Add "status" command
    Add status command to display information the current deployment
    of the OSTree repository when OSTree is deployed. This command
    will not work from a non-OSTree deployed system.
    
    The status command will display the current running branch,
    in green. The current commit that is deployed and the Debian
    version has been deployed.
    
    Also update tox.ini and bindep.txt to support ostree.
    
    Test Plan
    PASSED Installed apt-ostree from git repo.
    PASSED Run sudo apt-ostree compose create \
               --base config/debian/bookworm \
               --repo /repo debian/bookworm
    PASSED Run "apt-ostree compose image --base config/debian/image \
            --repo=/repo test"
    PASSED Start VM.
    PASSED Run the apt-ostree status command.
    
    Story: 2010867
    Task: 48556
    
    Change-Id: Ie55007e83869f5c491f97c1828508e7c8085f47a
    Signed-off-by: Charles Short <charles.short@windriver.com>

--- apt_ostree/ostree.py
+++ apt_ostree/ostree.py
@@ -6,8 +6,16 @@ SPDX-License-Identifier: Apache-2.0
 """
 
 import subprocess
+import sys
+
+import click
 
 from apt_ostree.utils import run_command
+
+# pylint: disable=wrong-import-position
+import gi
+gi.require_version("OSTree", "1.0")
+from gi.repository import Gio, GLib, OSTree  # noqa: H301
 
 
 class Ostree:
@@ -36,3 +44,48 @@ class Ostree:
         cmd += [str(root)]
         r = run_command(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         return r
+
+    def get_sysroot(self):
+        """Load the /ostree directory (sysroot)."""
+        sysroot = OSTree.Sysroot()
+        if not sysroot.load():
+            click.secho("Unable to load /sysroot", fg="red")
+            sys.exit(1)
+        return sysroot
+
+    def open_ostree(self):
+        """"Open the ostree repository."""
+        if self.state.repo:
+            repo = OSTree.Repo.new(Gio.File.new_for_path(str(self.state.repo)))
+            if not repo.open(None):
+                click.secho(
+                    "Opening the archive OSTree repository failed.", fg="red")
+                sys.exit(1)
+        else:
+            sysroot = self.get_sysroot()
+            _, repo = sysroot.get_repo()
+            if not repo.open():
+                click.secho(
+                    "Opening the archive OSTree repository failed.", fg="red")
+                sys.exit(1)
+        return repo
+
+    def get_branch(self):
+        """Get a branch in a current deployment."""
+        if self.state.branch:
+            return self.state.branch
+        else:
+            sysroot = self.get_sysroot()
+            deployment = sysroot.get_booted_deployment()
+            origin = deployment.get_origin()
+            try:
+                refspec = origin.get_string("origin", "refspec")
+            except GLib.Error as e:
+                # If not a "key not found" error then raise the exception
+                if not e.matches(GLib.KeyFile.error_quark(),
+                                 GLib.KeyFileError.KEY_NOT_FOUND):
+                    raise (e)
+                # Fallback to `baserefspec`
+                refspec = origin.get_string('origin', 'baserefspec')
+
+            return refspec
