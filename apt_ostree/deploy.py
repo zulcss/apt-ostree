406809503466e334b608223ac6a0e7e5177dda1b
Charles Short <charles.short@windriver.com>
2023-10-10 10:04:05 -0400

    Refactor prestaging/poststaging
    Refactor prestaging/poststaging of the ostree deployment so that it
    supports regular Ostree and StarlingX.
    
    Story: 2010867
    Task: 48556
    
    Test Plan:
    PASSED Installed apt-ostree from git repo.
    PASSED Run sudo apt-ostree compose create \
           --base config/debian/bookworm \
           --repo /repo debian/bookworm \
           bookworm-test
    PASSED Run sudo apt-ostree compose upgrade \
           --repo /repo --branch bookworm-test
    PASSED Check the output of "ostree log --repo /repo bookworm-test".
    
    Change-Id: I892bbb2b28cc929e22476efa682d8ed0dc812462
    Signed-off-by: Charles Short <charles.short@windriver.com>

--- apt_ostree/deploy.py
+++ apt_ostree/deploy.py
@@ -7,6 +7,7 @@ SPDX-License-Identifier: Apache-2.0
 
 import os
 import shutil
+import subprocess
 import sys
 
 import click
@@ -28,22 +29,59 @@ class Deploy:
         self.rootfs = None
 
     def prestaging(self, rootfs):
-        """Prestage steps."""
+        """Pre stage steps."""
+        if not rootfs.exists():
+            click.secho("rootfs not found: {rootfs}", fg="red")
+            sys.exit(1)
+
         with self.console.status("Running prestaging steps."):
-            # Ensure the directory is root:root.
-            os.chown(rootfs.joinpath("usr/etc"), 0, 0)
-            utils.run_sandbox_command(
-                ["systemd-tmpfiles", "--create"], rootfs
-            )
+            fd = os.open(rootfs,  os.O_DIRECTORY)
+            # Ensure that we correct permissions
+            os.chown(rootfs, 0, 0)
+            if not rootfs.joinpath("etc").exists():
+                os.symlink("usr/etc", "etc", dir_fd=fd)
+                os.chown(rootfs.joinpath("usr/etc"), 0, 0)
+            if rootfs.joinpath("usr/rootdirs/var/lib/dpkg").exists():
+                # /var is suppose to hold the state of the running
+                # system.
+                os.rmdir("var", dir_fd=fd)
+                os.symlink("usr/rootdirs/var", "var", dir_fd=fd)
+
+                r = utils.run_command(
+                    ["systemd-tmpfiles", f"--root={rootfs}", "--create",
+                     "--boot", "--prefix=/var"],
+                    stdout=subprocess.PIPE,
+                    stderr=subprocess.PIPE
+                )
+
+                # According to systemd-tmpfiles(8), the return values are:
+                #  0 - success
+                # 65 - so some lines had to be ignored, but no other errors
+                # 73 - configuration ok, but could not be created
+                #  1 - other error
+                if r.returncode not in [0, 65]:
+                    click.secho("Failed to populate /var,", fg="red")
+                    click.secho(f"Error code: {r.returncode}", fg="red")
+                    sys.exit(1)
 
     def poststaging(self, rootfs):
         """Post staging steps."""
+        if not rootfs.exists():
+            click.secho("rootfs not found: {rootfs}", fg="red")
+            sys.exit(1)
+
         self.console.print("Running post deploy steps.")
+        fd = os.open(rootfs, os.O_DIRECTORY)
+        if os.path.exists(rootfs.joinpath("etc")):
+            os.unlink("etc", dir_fd=fd)
         os.chown(rootfs.joinpath("usr/etc"), 0, 0)
-        # /var is stateless so remove everything that
-        # might be lying around.
-        shutil.rmtree(rootfs.joinpath("var"))
-        os.mkdir(rootfs.joinpath("var"), 0o755)
+        if rootfs.joinpath("usr/rootdirs/var/lib/dpkg").exists():
+            # /var is stateless so remove everything that
+            # might be lying around.
+            os.chown(rootfs.joinpath("usr/rootdirs/var"), 0, 0)
+            if rootfs.joinpath("var"):
+                os.unlink("var", dir_fd=fd)
+                os.mkdir("var", dir_fd=fd, mode=0o755)
 
     def cleanup(self, rootfs):
         """Remove workspace directores to save space."""
@@ -81,8 +119,8 @@ class Deploy:
         if update:
             self.console.print("Updating grub.")
             r = utils.run_command(
-                    ["update-grub"]
-                )
+                ["update-grub"]
+            )
             if r.returncode != 0:
                 click.secho("Failed to update grub.", fg="red")
                 sys.exit(1)
